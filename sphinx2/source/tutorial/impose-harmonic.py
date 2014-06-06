3ead8e3b85c912df16d42729b21c49118fef8fbe
Václav Šmilauer <eu@doxos.eu>
2014-06-06 20:02:35 +0200

    1. Go on with the tutorial, add the bottly.py script and partially write the chapter on imposewith examples and videos 2. SnapshotEngine.snapshots no longer shows up in the UI: when there was 1e3 items, the UI was refreshing very ofthen and RAM usage went up to 12GB and everything was very slow 3. Limit nesting for ObjectEditor (not clear, there is a freeze with clumps) 4. Add Mathr::UniformRandomRotation 5. RandomFactory defines padDist for better placing points randomly 6. FIx bug in CylinderFactory where the cylinder had unit radius 7. Add Particle::make (from shape and material) 8. Finish PharmaCapsuleGenerator

---
+++ sphinx2/source/tutorial/impose-harmonic.py
@@ -0,0 +1,32 @@
+import woo
+from woo import utils,pack
+from woo.dem import *
+from woo.core import *
+from minieigen import *
+
+woo.master.scene=S=Scene(fields=[DemField(gravity=(0,0,-10))])
+mat=utils.defaultMaterial()
+sp=pack.SpherePack()
+sp.makeCloud((4,4,4),(14,14,14),.4,rRelFuzz=.5)
+sp.toSimulation(S,mat=mat)
+S.dem.par.append([utils.wall(1,axis=2,sense=0,mat=mat,glAB=((-10,-1),(20,11))),])
+S.periodic=True
+S.cell.setBox(20,20,20)
+S.engines=utils.defaultEngines(damping=.4)
+S.dtSafety=.5
+S.dt=.5*utils.pWaveDt() # to compute oscillation freqs below
+# create cylinders
+for i,x in enumerate([-2,0,2,4,6,8,10.5,12,14]):
+	c=utils.infCylinder((x,0,3),radius=.8,axis=1,mat=mat,glAB=(-1,11))
+	c.angVel=(0,2.*(i+1),0)
+	# each of cylinders will move haronically along global x and z axes (not y)
+	c.impose=AlignedHarmonicOscillations(freqs=(1./(10000.*S.dt),float('nan'),1/(((i%3)+3)*1000.*S.dt)),amps=(.3*(i%2+1),0,.4*(i%4+1)))
+	S.dem.par.append(c)
+S.dem.collectNodes()
+S.saveTmp()
+
+try:
+	from woo import gl
+	gl.Gl1_Wall.div=10
+	gl.Gl1_InfCylinder.wire=True
+except ImportError: pass
